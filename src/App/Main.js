ac25467ac3cc3a30b0e0d06eb4e472e2844f57e4
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2023-08-25 22:42:09 +0200

    Update Main.js

--- src/App/Main.js
+++ src/App/Main.js
@@ -4,7 +4,7 @@ import './Main.css'; // Add styling for the panel
 const Main = () => {
   return (
     <div className="panel">
-      <img src="../images/your-image.jpg" alt="Your Image" />
+      <img src="../images/7410158.eps" alt="Your Image" />
       <button>Click Me</button>
     </div>
   );
