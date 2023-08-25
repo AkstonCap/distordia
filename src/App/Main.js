fa9b13fd359bfeb4d3fdcffc01aae71a51f3c41c
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2023-08-25 22:34:58 +0200

    Update Main.js

--- src/App/Main.js
+++ src/App/Main.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import './Main.css'; // Add styling for the panel
+
+const Main = () => {
+  return (
+    <div className="panel">
+      <img src="../images/your-image.jpg" alt="Your Image" />
+      <button>Click Me</button>
+    </div>
+  );
+};
+
+export default Main;
 
