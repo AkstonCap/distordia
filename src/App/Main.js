07c6955169d71e6401fafbfd1ef4849751b4f2e4
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-08-04 17:18:57 +0200

    Update Main.js

--- src/App/Main.js
+++ src/App/Main.js
@@ -1,14 +1,65 @@
-import React from 'react';
-import './Main.css'; // Add styling for the panel
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Panel, HorizontalTab } from 'nexus-module';
 
-const Main = () => {
+import * as TYPE from 'actions/types';
+import { refreshStatuses } from 'actions/market';
+
+import RefreshButton from './RefreshButton';
+import Summary from './Summary';
+import PriceAction from './PriceAction';
+import MarketDepth from './MarketDepth';
+
+export default function Main() {
+  const activeTab = useSelector((state) => state.ui.activeTab);
+  const dispatch = useDispatch();
+  const switchTab = (tab) => {
+    dispatch({
+      type: TYPE.SWITCH_TAB,
+      payload: tab,
+    });
+  };
+
+  useEffect(() => {
+    dispatch(refreshStatuses());
+  }, []);
+
   return (
-    <div className="panel">
-      <img src="../images/7410158.eps" alt="Your Image" />
-      <button>Click Me</button>
-    </div>
+    <Panel
+      controls={<RefreshButton />}
+      icon={{ url: 'icons/chart.svg', id: 'icon' }}
+      title={'Market Data'}
+    >
+      <HorizontalTab.TabBar>
+        <HorizontalTab
+          active={activeTab === 'summary'}
+          onClick={() => {
+            switchTab('summary');
+          }}
+        >
+          24hr Summary
+        </HorizontalTab>
+        <HorizontalTab
+          active={activeTab === 'price'}
+          onClick={() => {
+            switchTab('price');
+          }}
+        >
+          Price Chart
+        </HorizontalTab>
+        <HorizontalTab
+          active={activeTab === 'depth'}
+          onClick={() => {
+            switchTab('depth');
+          }}
+        >
+          Market Depth
+        </HorizontalTab>
+      </HorizontalTab.TabBar>
+
+      <div>{activeTab === 'summary' && <Summary />}</div>
+      <div>{activeTab === 'price' && <PriceAction />}</div>
+      <div>{activeTab === 'depth' && <MarketDepth />}</div>
+    </Panel>
   );
-};
-
-export default Main;
-
+}
