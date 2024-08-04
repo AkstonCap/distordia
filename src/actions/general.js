ecb48146e38fe1639f85ea17009b22ecefc4e38f
AkstonCap <88211167+AkstonCap@users.noreply.github.com>
2024-08-04 15:33:19 +0200

    Create general.js

---
+++ src/actions/general.js
@@ -0,0 +1,16 @@
+import * as TYPE from './types';
+
+export const initialize = data => ({
+  type: TYPE.INITIALIZE,
+  payload: data,
+});
+
+export const updateCoreInfo = coreInfo => ({
+  type: TYPE.UPDATE_CORE_INFO,
+  payload: coreInfo,
+});
+
+export const updateTheme = theme => ({
+  type: TYPE.UPDATE_THEME,
+  payload: theme,
+});
